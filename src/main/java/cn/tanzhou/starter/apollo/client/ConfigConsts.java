11744be3af64885a915ef8a8b17ee31047ac9cad
DimonHo <vampirehgg@hotmail.com>
2020-12-28 15:23:48 +0800

    Create ConfigConsts.java

---
+++ src/main/java/cn/tanzhou/starter/apollo/client/ConfigConsts.java
@@ -0,0 +1,18 @@
+package cn.tanzhou.starter.apollo.client;
+
+/**
+ * @author 敖癸
+ * @date 2020/12/17 - 16:13
+ */
+public interface ConfigConsts extends com.ctrip.framework.apollo.core.ConfigConsts {
+
+    String APOLLO_ID_KEY = "app.id";
+
+    String SPRING_PROFILES_ACTIVE_KEY = "spring.profiles.active";
+
+    String SPRING_APPLICATION_NAME_KEY = "spring.application.name";
+
+    String APOLLO_CACHE_DIR_KEY = "apollo.cacheDir";
+
+    String APOLLO_ACCESSKEY_SECRET_KEY = "apollo.accesskey.secret";
+}
